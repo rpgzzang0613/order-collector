da713d6adf96fb03635666943ac7f22a2fe722f5
bsho88 <rpgzzang@naver.com>
2023-12-10 15:13:02 +0900

    RestController에서 일반 Controller로 수정, Index용 컨트롤러 추가

---
+++ src/main/java/com/amiiboroom/ordercollector/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.amiiboroom.ordercollector.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(HttpSession session) {
+        HashMap<String, Object> user = (HashMap<String, Object>) session.getAttribute("user");
+        if(user == null || user.isEmpty()) {
+            return "redirect:/login";
+        }
+
+        return "index";
+    }
+
+}
--- src/main/java/com/amiiboroom/ordercollector/controller/UserController.java
+++ src/main/java/com/amiiboroom/ordercollector/controller/UserController.java
@@ -6,31 +6,51 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
 
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "users/signup";
+    }
+
     @PostMapping("/signup")
-    public ResponseEntity<ApiResult> signup(@RequestBody HashMap<String, Object> requestMap) {
+    @ResponseBody
     public ResponseEntity<BackendResult> signup(@RequestBody HashMap<String, Object> requestMap) {
         return userService.signup(requestMap);
     }
 
     @GetMapping("/login")
-    public ResponseEntity<ApiResult> login(@RequestBody HashMap<String, Object> requestMap, HttpSession session) {
-        return userService.login(requestMap);
+    public String loginPage(HttpSession session) {
+        HashMap<String, Object> user = (HashMap<String, Object>) session.getAttribute("user");
+
+        if(user != null) {
+            return "redirect:/";
+        }
+
+        return "users/login";
+    }
+
     @PostMapping("/login")
     @ResponseBody
     public ResponseEntity<BackendResult> login(@RequestBody HashMap<String, Object> requestMap, HttpSession session) {
         ResponseEntity<BackendResult> response = userService.login(requestMap);
+
+        HashMap<String, Object> userMap = (HashMap<String, Object>) response.getBody().getData();
+
+        if(userMap != null) {
+            session.setAttribute("user", userMap);
+        }
 
         return response;
     }
