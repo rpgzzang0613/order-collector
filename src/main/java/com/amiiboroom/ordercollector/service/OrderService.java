96c86b2b576674ad01c9a7ee6251d444a5886479
bsho88 <rpgzzang0613@gmail.com>
2024-01-29 16:56:11 +0900

    안쓰는 import 제거

--- src/main/java/com/amiiboroom/ordercollector/service/OrderService.java
+++ src/main/java/com/amiiboroom/ordercollector/service/OrderService.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
