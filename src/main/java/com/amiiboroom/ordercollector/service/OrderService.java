679de46b21dfeb19b637a4b482d3c6fbb500de25
bsho88 <rpgzzang@naver.com>
2024-01-31 11:17:21 +0900

    미사용 코드 제거

--- src/main/java/com/amiiboroom/ordercollector/service/OrderService.java
+++ src/main/java/com/amiiboroom/ordercollector/service/OrderService.java
@@ -102,18 +102,4 @@ public class OrderService {
 
         return ResponseEntity.ok(backendResult);
     }
-
-    private WebDriver initWebDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");          // 최대 창크기로 실행 (안보일때도 적용됨)
-        options.addArguments("--headless=new");             // 안보이고 백그라운드에서 동작하게 설정
-        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-logging"));  // 불필요 로그 제거
-
-        WebDriverManager.chromedriver().setup();            // 크롬드라이버 버전 체크 및 매칭
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(13L)); // 페이지 대기시간 13초 (넘어가도록 응답없으면 예외발생)
-
-        return driver;
-    }
-
 }
