ec0c3631d8081ffa3df8db9cd3ceab433e936e29
bsho88 <rpgzzang@naver.com>
2024-02-17 15:30:57 +0900

    test2

--- src/main/java/com/amiiboroom/ordercollector/controller/MainController.java
+++ src/main/java/com/amiiboroom/ordercollector/controller/MainController.java
@@ -22,7 +22,7 @@ public class MainController {
     public String indexPage(HttpSession session) {
         String urlPath = "redirect:/accounts/login";
 
-        HashMap<String, Object> user = (HashMap<String, Object>) session.getAttribute("user");//test
+        HashMap<String, Object> user = (HashMap<String, Object>) session.getAttribute("user");
         if(user != null && !user.isEmpty()) {
             urlPath = "redirect:/dashboard";
         }
